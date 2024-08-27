73f5e73e4322aba0df12d42a44526f3fb6446b5d
Wesley Henckel <165380025+WesleyHenckel@users.noreply.github.com>
2024-08-27 14:14:21 -0300

    Update urls.py

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.index, name='index'),  # Rota padrão
     path('cadastro/', views.cadastro, name="cadastro"),
     path('logar/', views.logar, name='logar'),
 ]
